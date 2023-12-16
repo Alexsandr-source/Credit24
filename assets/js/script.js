b4728439653064403aa13f44c700d66a5d95298f
Toni01~ <neonver.2020@gmail.com>
2023-12-16 14:41:10 +0900

    :wq

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+const changeLanguage = document.querySelectorAll('.btn-language');
+const languageRu = document.querySelector('#languageRu');
+const languageEn = document.querySelector('#languageEn');
+let daysLost = document.querySelector('#daysLost');
+
+
+// OUR SERVICES button //
+// Set event handlers for buttons
+languageRu.addEventListener('click', changesLanguage);
+languageEn.addEventListener('click', changesLanguage);
+// Function for updating the buttons
+function changesLanguage() {
+    for(var i = 0; i < changeLanguage.length; i++) {
+        changeLanguage[i].classList.remove('active');
+    }
+    if (this.className === ('btn-language active')) {
+        this.classList.remove('active')
+    } else {
+        this.classList.add('active');
+    }
+};
+
+daysLost.
