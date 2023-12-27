c45675b5b0e101b3a11af1e98e4966412d8ab503
Toni01~ <neonver.2020@gmail.com>
2023-12-27 16:34:51 +0900

    :wq

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,27 @@ const languageRu = document.querySelector('#languageRu');
 const languageEn = document.querySelector('#languageEn');
 const prevPartners = document.querySelector('#prevPartners');
 const nextPartners = document.querySelector('#nextPartners');
+const prevReviews = document.querySelector('#prevReviews');
+const nextReviews = document.querySelector('#nextReviews');
 const partnersSlider = document.querySelector('.partners__slider-line');
 const partnersSlides = Array.from(partnersSlider.querySelectorAll('.partners__slide'));
+const reviewsSlider = document.querySelector('.reviews__slider-line');
+const reviewsSlides = Array.from(reviewsSlider.querySelectorAll('.reviews__slide'));
+const reviewsWrapper = document.querySelector('#reviewsWrapper');
+const reviewsWrappers = Array.from(reviewsWrapper.querySelectorAll('.reviews__btn'));
+
 let partnersCount = partnersSlides.length;
 let partnersIndex = 0;
-console.log(partnersCount);
+let reviewsCount = reviewsSlides.length;
+let reviewsIndex = 0;
+let wrapperCount = reviewsWrappers.length;
+let wrapperIndex = 0;
 
-// changes button //
+// Language //
 languageRu.addEventListener('click', changesLanguage);
 languageEn.addEventListener('click', changesLanguage);
 function changesLanguage() {
-    for(var i = 0; i < changeLanguage.length; i++) {
+    for(var i = 0; i < changeLanguage.length; ++i) {
         changeLanguage[i].classList.remove('active');
     }
     if (this.className === ('btn-language active')) {
@@ -41,20 +51,17 @@ function updateCountndown() {
     time--;
 }
 
-//sliderPartners
+//Partners
 prevPartners.addEventListener('click', prevPartnersSlide);
 nextPartners.addEventListener('click', nextPartnersSlide);
-
 function prevPartnersSlide() {
-    partnersIndex = (partnersIndex - 1 + partnersCount) % partnersCount;
+    partnersIndex = (--partnersIndex + partnersCount) % partnersCount;
     updatePartner();
 }
-
 function nextPartnersSlide() {
-    partnersIndex = (partnersIndex + 1) % partnersCount;
+    partnersIndex = (++partnersIndex) % partnersCount;
     updatePartner();
 }
-
 function updatePartner() {
     partnersSlides.forEach((slide, index) => {
     if (index === partnersIndex) {
@@ -64,5 +71,44 @@ function updatePartner() {
     }
 });
 }
-
 updatePartner();
+
+//Reviews
+prevReviews.addEventListener('click', prevReviewsSlide);
+nextReviews.addEventListener('click', nextReviewsSlide);
+function prevReviewsSlide() {
+    reviewsIndex = (--reviewsIndex + reviewsCount) % reviewsCount;
+    --wrapperIndex
+    updateWrapper(wrapperIndex)
+    updateReviews();
+}
+function nextReviewsSlide() {
+    reviewsIndex = (++reviewsIndex) % reviewsCount;
+    ++wrapperIndex
+    updateWrapper(wrapperIndex)
+    updateReviews();
+}
+function updateReviews() {
+    reviewsSlides.forEach((slide, index) => {
+        if (index === reviewsIndex) {
+            slide.style.display = 'flex';
+        } else {
+            slide.style.display = 'none';
+        }
+    });
+}
+updateReviews();
+
+reviewsWrappers.forEach((wrapper, index) => {
+    wrapper.addEventListener('click', () => {
+        reviewsIndex = index;
+        updateReviews();
+    })
+})
+
+const updateWrapper = (index) => {
+    for (let wrapper in reviewsWrappers) {
+        wrapper.classList.remove('reviews__btn-active')
+    }
+    reviewsWrappers[index].classList.add('reviews__btn-active')
+}
